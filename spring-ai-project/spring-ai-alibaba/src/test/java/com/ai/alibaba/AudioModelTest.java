4d644311a0757466d9bbaee70be5b6caa4affb76
cz2925928087 <2925928087@qq.com>
2026-09-16 09:24:46 +0800

    Add native DashScope TTS synthesis test

--- spring-ai-project/spring-ai-alibaba/src/test/java/com/ai/alibaba/AudioModelTest.java
+++ spring-ai-project/spring-ai-alibaba/src/test/java/com/ai/alibaba/AudioModelTest.java
@@ -4,6 +4,12 @@ import com.alibaba.cloud.ai.dashscope.audio.DashScopeSpeechSynthesisModel;
 import com.alibaba.cloud.ai.dashscope.audio.DashScopeSpeechSynthesisOptions;
 import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisPrompt;
 import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisResponse;
+import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisResult;
+import com.alibaba.dashscope.audio.tts.SpeechSynthesisParam;
+
+import com.alibaba.dashscope.audio.tts.SpeechSynthesizer;
+import com.alibaba.dashscope.common.ResultCallback;
+import com.alibaba.dashscope.utils.Constants;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +18,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootTest
 public class AudioModelTest {
@@ -51,6 +58,30 @@ public class AudioModelTest {
             fos.write(audio.array());
         } catch (IOException e) {
             System.out.println("写入文件失败");
+        }
+    }
+
+    @Test
+    void test() {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        SpeechSynthesisParam param = SpeechSynthesisParam.builder()
+                // 若没有将API Key配置到环境变量中，需将下面这行代码注释放开，并将apiKey替换为自己的API Key
+                // .apiKey("yourApikey")
+                .model("sambert-zhichu-v1")
+                .text("今天天气怎么样")
+                .sampleRate(48000)
+                .enableWordTimestamp(true)
+                .enablePhonemeTimestamp(true)
+                .build();
+
+        File file = new File(System.getProperty("user.dir")+"/output.WAV");
+        ByteBuffer audio = synthesizer.call(param);
+
+        try(FileOutputStream fos = new FileOutputStream(file)){
+            fos.write(audio.array());
+            System.out.println("sunthesis done");
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
     }
 }
